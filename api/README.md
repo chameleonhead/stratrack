ef7fb79734d2a525ef881fe4898befe993500b21
Yuto Nagano <chamelion@hotmail.co.jp>
2025-06-21 18:55:26 +0900

    ローカル環境でviteとazure functionsを結合

---
+++ api/README.md
@@ -0,0 +1,6 @@
+# 準備
+
+1. Azure Functions Core Tools のインストール
+2. `cd Stratrack.Api` を実行する
+3. `func start` を実行する
+4. http://localhost:7071/api/swagger/ui にアクセスする
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,17 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:7071",
+        changeOrigin: true,
+      },
+    },
+  },
+});
