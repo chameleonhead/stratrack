2533cd01eb2a8099546480d58bb5e2f74d30e36a
Yuto Nagano <chamelion@hotmail.co.jp>
2025-06-29 13:02:12 +0900

    Implement Dukascopy client using LZMA library

--- api/Stratrack.Api/Infrastructure/DukascopyClient.cs
+++ api/Stratrack.Api/Infrastructure/DukascopyClient.cs
@@ -1,11 +1,11 @@
 using System.Buffers.Binary;
-using System.Diagnostics;
+using LzmaDecoder = SevenZip.Compression.LZMA.Decoder;
 using System.Text;
 using Stratrack.Api.Domain.Dukascopy;
 
 namespace Stratrack.Api.Infrastructure;
 
-public class StubDukascopyClient : IDukascopyClient
+public class DukascopyClient : IDukascopyClient
 {
     private readonly HttpClient _http = new();
 
@@ -15,18 +15,16 @@ public class StubDukascopyClient : IDukascopyClient
         var url = $"https://datafeed.dukascopy.com/datafeed/{symbol}/{baseTime:yyyy}/{baseTime:MM}/{baseTime:dd}/{baseTime:HH}h_ticks.bi5";
         var compressed = await _http.GetByteArrayAsync(url, token).ConfigureAwait(false);
 
-        var psi = new ProcessStartInfo("lzma", "-dc")
-        {
-            RedirectStandardInput = true,
-            RedirectStandardOutput = true,
-            UseShellExecute = false
-        };
-        using var proc = Process.Start(psi)!;
-        await proc.StandardInput.BaseStream.WriteAsync(compressed, token).ConfigureAwait(false);
-        proc.StandardInput.Close();
+        using var inStream = new MemoryStream(compressed);
+        var properties = new byte[5];
+        await inStream.ReadAsync(properties, token).ConfigureAwait(false);
+        var decoder = new LzmaDecoder();
+        decoder.SetDecoderProperties(properties);
+        var outSizeBytes = new byte[8];
+        await inStream.ReadAsync(outSizeBytes, token).ConfigureAwait(false);
+        var outSize = BitConverter.ToInt64(outSizeBytes, 0);
         using var decompressed = new MemoryStream();
-        await proc.StandardOutput.BaseStream.CopyToAsync(decompressed, token).ConfigureAwait(false);
-        await proc.WaitForExitAsync(token).ConfigureAwait(false);
+        decoder.Code(inStream, decompressed, inStream.Length - inStream.Position, outSize, null);
 
         var span = decompressed.ToArray();
         var sb = new StringBuilder();
