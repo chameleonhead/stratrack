2f961d5e5ba868b2356a82a488a0e05fb6118980
Yuto Nagano <chamelion@hotmail.co.jp>
2025-08-19 04:19:54 +0900

    feat: detail chart operation signatures

--- libs/mql-interpreter/src/libs/signatures.ts
+++ libs/mql-interpreter/src/libs/signatures.ts
@@ -305,74 +305,143 @@ const arrayBuiltinSignatures: BuiltinSignaturesMap = {
 // https://docs.mql4.com/chart_operations
 const chartOperationsBuiltinSignatures: BuiltinSignaturesMap = {
   ChartApplyTemplate: {
-    args: [],
-    returnType: "int",
-    description: "Applies a specific template from a specified file to the chart",
+    args: [
+      { name: "chart_id", type: "long" },
+      { name: "filename", type: "string" },
+    ],
+    returnType: "bool",
+    description: "Applies a specific template from a file to the chart",
   },
   ChartClose: {
-    args: [],
-    returnType: "int",
+    args: [{ name: "chart_id", type: "long", optional: true }],
+    returnType: "bool",
     description: "Closes the specified chart",
   },
   ChartFirst: {
     args: [],
-    returnType: "int",
+    returnType: "long",
     description: "Returns the ID of the first chart of the client terminal",
   },
-  ChartGetDouble: {
-    args: [],
-    returnType: "double",
-    description: "Returns the double value property of the specified chart",
-  },
-  ChartGetInteger: {
-    args: [],
-    returnType: "int",
-    description: "Returns the integer value property of the specified chart",
-  },
-  ChartGetString: {
-    args: [],
-    returnType: "string",
-    description: "Returns the string value property of the specified chart",
-  },
+  ChartGetDouble: [
+    {
+      args: [
+        { name: "chart_id", type: "long" },
+        { name: "prop_id", type: "int" },
+        { name: "sub_window", type: "int", optional: true },
+      ],
+      returnType: "double",
+      description: "Returns the double value property of the specified chart",
+    },
+    {
+      args: [
+        { name: "chart_id", type: "long" },
+        { name: "prop_id", type: "int" },
+        { name: "sub_window", type: "int" },
+        { name: "double_var", type: "double" },
+      ],
+      returnType: "bool",
+      description: "Retrieves a double chart property into a reference variable",
+    },
+  ],
+  ChartGetInteger: [
+    {
+      args: [
+        { name: "chart_id", type: "long" },
+        { name: "prop_id", type: "int" },
+        { name: "sub_window", type: "int", optional: true },
+      ],
+      returnType: "long",
+      description: "Returns the integer value property of the specified chart",
+    },
+    {
+      args: [
+        { name: "chart_id", type: "long" },
+        { name: "prop_id", type: "int" },
+        { name: "sub_window", type: "int" },
+        { name: "long_var", type: "long" },
+      ],
+      returnType: "bool",
+      description: "Retrieves an integer chart property into a reference variable",
+    },
+  ],
+  ChartGetString: [
+    {
+      args: [
+        { name: "chart_id", type: "long" },
+        { name: "prop_id", type: "int" },
+        { name: "sub_window", type: "int", optional: true },
+      ],
+      returnType: "string",
+      description: "Returns the string value property of the specified chart",
+    },
+    {
+      args: [
+        { name: "chart_id", type: "long" },
+        { name: "prop_id", type: "int" },
+        { name: "sub_window", type: "int" },
+        { name: "string_var", type: "string" },
+      ],
+      returnType: "bool",
+      description: "Retrieves a string chart property into a reference variable",
+    },
+  ],
   ChartID: {
     args: [],
-    returnType: "int",
+    returnType: "long",
     description: "Returns the ID of the current chart",
   },
   ChartIndicatorDelete: {
-    args: [],
-    returnType: "int",
+    args: [
+      { name: "chart_id", type: "long" },
+      { name: "window_index", type: "int" },
+      { name: "indicator_shortname", type: "string" },
+    ],
+    returnType: "bool",
     description: "Removes an indicator with a specified name from the specified chart window",
   },
   ChartIndicatorName: {
-    args: [],
+    args: [
+      { name: "chart_id", type: "long" },
+      { name: "window_index", type: "int" },
+      { name: "index", type: "int" },
+    ],
     returnType: "string",
     description:
       "Returns the short name of the indicator by the number in the indicators list on the specified chart window",
   },
   ChartIndicatorsTotal: {
-    args: [],
+    args: [
+      { name: "chart_id", type: "long" },
+      { name: "window_index", type: "int" },
+    ],
     returnType: "int",
     description: "Returns the number of all indicators applied to the specified chart window.",
   },
   ChartNavigate: {
-    args: [],
-    returnType: "int",
+    args: [
+      { name: "chart_id", type: "long" },
+      { name: "position", type: "int" },
+      { name: "shift", type: "int" },
+    ],
+    returnType: "bool",
     description:
       "Performs shift of the specified chart by the specified number of bars relative to the specified position in the chart",
   },
   ChartNext: {
-    args: [],
-    returnType: "int",
+    args: [{ name: "chart_id", type: "long" }],
+    returnType: "long",
     description: "Returns the chart ID of the chart next to the specified one",
   },
   ChartOpen: {
-    args: [],
-    returnType: "int",
+    args: [
+      { name: "symbol", type: "string" },
+      { name: "period", type: "int" },
+    ],
+    returnType: "long",
     description: "Opens a new chart with the specified symbol and period",
   },
   ChartPeriod: {
-    args: [],
+    args: [{ name: "chart_id", type: "long", optional: true }],
     returnType: "int",
     description: "Returns the period value of the specified chart",
   },
@@ -388,38 +457,63 @@ const chartOperationsBuiltinSignatures: BuiltinSignaturesMap = {
     description: "Calls a forced redrawing of a specified chart",
   },
   ChartSaveTemplate: {
-    args: [],
-    returnType: "int",
+    args: [
+      { name: "chart_id", type: "long" },
+      { name: "filename", type: "string" },
+    ],
+    returnType: "bool",
     description: "Saves current chart settings in a template with a specified name",
   },
   ChartScreenShot: {
-    args: [],
-    returnType: "int",
-    description: "Provides a screenshot of the chart of its current state in a gif format",
+    args: [
+      { name: "chart_id", type: "long" },
+      { name: "filename", type: "string" },
+      { name: "width", type: "int" },
+      { name: "height", type: "int" },
+      { name: "align_mode", type: "int", optional: true },
+    ],
+    returnType: "bool",
+    description: "Provides a screenshot of the chart of its current state",
   },
   ChartSetDouble: {
-    args: [],
-    returnType: "int",
+    args: [
+      { name: "chart_id", type: "long" },
+      { name: "prop_id", type: "int" },
+      { name: "value", type: "double" },
+    ],
+    returnType: "bool",
     description: "Sets the double value for a corresponding property of the specified chart",
   },
   ChartSetInteger: {
-    args: [],
-    returnType: "int",
+    args: [
+      { name: "chart_id", type: "long" },
+      { name: "prop_id", type: "int" },
+      { name: "value", type: "long" },
+    ],
+    returnType: "bool",
     description:
       "Sets the integer value (datetime, int, color, bool or char) for a corresponding property of the specified chart",
   },
   ChartSetString: {
-    args: [],
-    returnType: "int",
+    args: [
+      { name: "chart_id", type: "long" },
+      { name: "prop_id", type: "int" },
+      { name: "value", type: "string" },
+    ],
+    returnType: "bool",
     description: "Sets the string value for a corresponding property of the specified chart",
   },
   ChartSetSymbolPeriod: {
-    args: [],
-    returnType: "int",
+    args: [
+      { name: "chart_id", type: "long" },
+      { name: "symbol", type: "string" },
+      { name: "period", type: "int" },
+    ],
+    returnType: "bool",
     description: "Changes the symbol value and a period of the specified chart",
   },
   ChartSymbol: {
-    args: [],
+    args: [{ name: "chart_id", type: "long", optional: true }],
     returnType: "string",
     description: "Returns the symbol name of the specified chart",
   },
@@ -430,13 +524,23 @@ const chartOperationsBuiltinSignatures: BuiltinSignaturesMap = {
       "Returns the time coordinate of the chart point, the Expert Advisor or script has been dropped to",
   },
   ChartTimePriceToXY: {
-    args: [],
-    returnType: "int",
+    args: [
+      { name: "chart_id", type: "long" },
+      { name: "sub_window", type: "int" },
+      { name: "time", type: "datetime" },
+      { name: "price", type: "double" },
+      { name: "x", type: "int" },
+      { name: "y", type: "int" },
+    ],
+    returnType: "bool",
     description:
       "Converts the coordinates of a chart from the time/price representation to the X and Y coordinates",
   },
   ChartWindowFind: {
-    args: [],
+    args: [
+      { name: "chart_id", type: "long" },
+      { name: "indicator_shortname", type: "string" },
+    ],
     returnType: "int",
     description: "Returns the number of a subwindow where an indicator is drawn",
   },
@@ -453,8 +557,15 @@ const chartOperationsBuiltinSignatures: BuiltinSignaturesMap = {
       "Returns the X coordinate of the chart point, the Expert Advisor or script has been dropped to",
   },
   ChartXYToTimePrice: {
-    args: [],
-    returnType: "int",
+    args: [
+      { name: "chart_id", type: "long" },
+      { name: "sub_window", type: "int" },
+      { name: "x", type: "int" },
+      { name: "y", type: "int" },
+      { name: "time", type: "datetime" },
+      { name: "price", type: "double" },
+    ],
+    returnType: "bool",
     description: "Converts the X and Y coordinates on a chart to the time and price values",
   },
   ChartYOnDropped: {
