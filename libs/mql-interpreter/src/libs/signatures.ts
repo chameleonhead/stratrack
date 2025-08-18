eaff99bd7038aa86378e4325d755dd006b5267c9
Yuto Nagano <chamelion@hotmail.co.jp>
2025-08-19 07:56:36 +0900

    chore: define global variable builtin signatures

--- libs/mql-interpreter/src/libs/signatures.ts
+++ libs/mql-interpreter/src/libs/signatures.ts
@@ -1571,44 +1571,64 @@ const filesBuiltinSignatures: BuiltinSignaturesMap = {
 // https://docs.mql4.com/globals
 const globalsBuiltinSignatures: BuiltinSignaturesMap = {
   GlobalVariableCheck: {
-    args: [],
+    args: [{ name: "name", type: "string" }],
     returnType: "bool",
     description: "Checks the existence of a global variable with the specified name",
   },
   GlobalVariableDel: {
-    args: [],
+    args: [{ name: "name", type: "string" }],
     returnType: "bool",
     description: "Deletes a global variable",
   },
-  GlobalVariableGet: {
-    args: [],
-    returnType: "double",
-    description: "Returns the value of a global variable",
-  },
+  GlobalVariableGet: [
+    {
+      args: [{ name: "name", type: "string" }],
+      returnType: "double",
+      description: "Returns the value of a global variable",
+    },
+    {
+      args: [
+        { name: "name", type: "string" },
+        { name: "double_var", type: "double" },
+      ],
+      returnType: "bool",
+      description: "Copies the value of a global variable into the provided variable",
+    },
+  ],
   GlobalVariableName: {
-    args: [],
+    args: [{ name: "index", type: "int" }],
     returnType: "string",
     description:
       "Returns the name of a global variable by it's ordinal number in the list of global variables",
   },
   GlobalVariablesDeleteAll: {
-    args: [],
-    returnType: "bool",
+    args: [
+      { name: "prefix_name", type: "string", optional: true },
+      { name: "limit_data", type: "datetime", optional: true },
+    ],
+    returnType: "int",
     description: "Deletes global variables with the specified prefix in their names",
   },
   GlobalVariableSet: {
-    args: [],
-    returnType: "bool",
+    args: [
+      { name: "name", type: "string" },
+      { name: "value", type: "double" },
+    ],
+    returnType: "datetime",
     description: "Sets the new value to a global variable",
   },
   GlobalVariableSetOnCondition: {
-    args: [],
+    args: [
+      { name: "name", type: "string" },
+      { name: "value", type: "double" },
+      { name: "check_value", type: "double" },
+    ],
     returnType: "bool",
     description: "Sets the new value of the existing global variable by condition",
   },
   GlobalVariablesFlush: {
     args: [],
-    returnType: "bool",
+    returnType: "void",
     description: "Forcibly saves contents of all global variables to a disk",
   },
   GlobalVariablesTotal: {
@@ -1617,13 +1637,13 @@ const globalsBuiltinSignatures: BuiltinSignaturesMap = {
     description: "Returns the total number of global variables",
   },
   GlobalVariableTemp: {
-    args: [],
+    args: [{ name: "name", type: "string" }],
     returnType: "bool",
     description:
       "Sets the new value to a global variable, that exists only in the current session of the terminal",
   },
   GlobalVariableTime: {
-    args: [],
+    args: [{ name: "name", type: "string" }],
     returnType: "datetime",
     description: "Returns time of the last accessing the global variable",
   },
